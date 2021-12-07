92efb70baf2604f76675a529c809eee16747faae
rrtrrtrrt <95685637+rrtrrtrrt@users.noreply.github.com>
2021-12-07 12:04:00 +0000

    Added day 2

---
+++ Day 2/Day 2.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  7 11:41:48 2021
+
+@author: User
+"""
+# Part 1
+
+ver_pos = 0
+hor_pos = 0
+
+with open('input.txt') as f:
+    for line in f:
+        if line.startswith('forward'):
+            hor_pos = hor_pos + int(line.split(' ')[1])
+        elif line.startswith('up'):
+            ver_pos = ver_pos - int(line.split(' ')[1])
+        elif line.startswith('down'):
+            ver_pos = ver_pos + int(line.split(' ')[1])
+        else:
+            pass
+        
+print(ver_pos*hor_pos)
+
+
+# Part 2
