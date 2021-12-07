024b00a58730bb0b4eb9b1fd35055aa19f7a0439
rrtrrtrrt <95685637+rrtrrtrrt@users.noreply.github.com>
2021-12-07 12:09:37 +0000

    Update Day 2.py

--- Day 2/Day 2.py
+++ Day 2/Day 2.py
@@ -24,3 +24,21 @@ print(ver_pos*hor_pos)
 
 
 # Part 2
+
+ver_pos = 0
+hor_pos = 0
+aim = 0
+
+with open('input.txt') as f:
+    for line in f:
+        if line.startswith('forward'):
+            hor_pos = hor_pos + int(line.split(' ')[1])
+            ver_pos = ver_pos + aim * int(line.split(' ')[1])
+        elif line.startswith('up'):
+            aim = aim - int(line.split(' ')[1])
+        elif line.startswith('down'):
+            aim = aim + int(line.split(' ')[1])
+        else:
+            pass
+        
+print(ver_pos*hor_pos)
