ccc077e97c491ba43ff0696198083ba8f5be862b
rrtrrtrrt <95685637+rrtrrtrrt@users.noreply.github.com>
2021-12-07 11:38:12 +0000

    Update Day1.py

--- Day 1/Day1.py
+++ Day 1/Day1.py
@@ -5,6 +5,17 @@ Created on Tue Dec  7 10:08:41 2021
 @author: User
 """
 
+# Part 1
+
 with open('input.txt') as f:
     input_list = [int(line) for line in f]
 print(sum(input_list[i+1]-input_list[i] > 0 for i in range(len(input_list)-1)))
+
+
+# Part 2
+
+rolling_window = []
+for i in range(len(input_list)-2):
+    rolling_window.append(input_list[i+2]+input_list[i+1]+input_list[i])
+    
+print(sum(rolling_window[i+1]-rolling_window[i] > 0 for i in range(len(rolling_window)-1)))
